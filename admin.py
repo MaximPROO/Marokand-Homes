921ff192e2f27a927f200c3462971ef11661ae12
Туракулов Мухаммад <138586523+MaximPROO@users.noreply.github.com>
2025-06-15 19:22:55 +0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -31,16 +31,49 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Главная страница, которая отображает список пользователей."""
+    """Главная страница, которая отображает список пользователей с сортировкой."""
     session = Session()
     try:
+        # Получаем параметры сортировки из URL
+        sort_by = request.args.get('sort', 'registration_date')  # По умолчанию сортируем по дате
+        order = request.args.get('order', 'desc')  # По умолчанию по убыванию
+        
         total_users = session.query(User).count()
-        all_users = session.query(User).order_by(User.registration_date.desc()).all()
-        return render_template('index.html', users=all_users, total_users=total_users)
+        subscribed_users = session.query(User).filter(User.is_subscribed == True).count()
+        
+        # Определяем поле для сортировки
+        if sort_by == 'referral_count':
+            sort_field = User.referral_count
+        else:
+            sort_field = User.registration_date
+        
+        # Применяем сортировку
+        if order == 'asc':
+            all_users = session.query(User).order_by(sort_field.asc()).all()
+        else:
+            all_users = session.query(User).order_by(sort_field.desc()).all()
+        
+        return render_template('index.html', 
+                             users=all_users, 
+                             total_users=total_users,
+                             subscribed_users=subscribed_users,
+                             current_sort=sort_by,
+                             current_order=order)
     finally:
         session.close()
 
-# --- НОВЫЙ МАРШРУТ ДЛЯ ДЕТАЛЬНОЙ ИНФОРМАЦИИ ---
+@app.route('/top50')
+def top50():
+    """Страница TOP-50 - пользователи с 50+ рефералами."""
+    session = Session()
+    try:
+        # Находим пользователей с 50+ рефералами, сортируем по количеству рефералов
+        top_users = session.query(User).filter(User.referral_count >= 50).order_by(User.referral_count.desc()).all()
+        
+        return render_template('top50.html', top_users=top_users, total_top=len(top_users))
+    finally:
+        session.close()
+
 @app.route('/user/<int:user_id>')
 def user_details(user_id):
     """Страница с детальной информацией о пользователе и его рефералах."""
